377b739950c5ac4c65c85fa7742ae6ba414e949b
Muhammad <md.unicorn@gmail.com>
2013-04-14 12:17:54 +0430

    `ViewModelBase`

---
+++ SepidarSidebar/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace SG.SepidarSidebar
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propName)
+        {
+            var h = PropertyChanged;
+            if (h != null)
+                h(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
