66075ea3548eacbbee4e2a814c9ea862b8cfaca6
Muhammad <md.unicorn@gmail.com>
2013-04-14 12:27:42 +0430

    ClockControl added with unfinished background.

---
+++ SepidarSidebar/ClockControlViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SG.SepidarSidebar
+{
+    public class ClockControlViewModel : ViewModelBase
+    {
+        #region Fields
+        #endregion
+
+        public ClockControlViewModel()
+        {
+        }
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+
+        #endregion
+
+    }
+}
