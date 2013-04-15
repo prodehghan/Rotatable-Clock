2c792b561ba590a3abe96fa2d8d4262086cde8d5
Muhammad <md.unicorn@gmail.com>
2013-04-15 23:30:02 +0430

    Fancy keyboard shortcuts.
    * Up, Down, Left, Right : Rotate clock plane
    * +/- : Zoom in and out
    * 0 : Reset all

--- SepidarSidebar/MainWindow.xaml.cs
+++ SepidarSidebar/MainWindow.xaml.cs
@@ -20,9 +20,13 @@ namespace SG.SepidarSidebar
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainWindowViewModel _viewModel;
         public MainWindow()
         {
             InitializeComponent();
+
+            _viewModel = new MainWindowViewModel();
+            this.DataContext = _viewModel;
         }
 
         protected override void OnMouseDown(MouseButtonEventArgs e)
@@ -31,6 +35,57 @@ namespace SG.SepidarSidebar
 
             if (e.LeftButton == MouseButtonState.Pressed && e.RightButton == MouseButtonState.Released)
                 this.DragMove();
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.Key == Key.D0)
+                ResetTransforms();
+            else if (e.Key == Key.OemPlus)
+            {
+                double w = this.Width * 1.5;
+                double sw = SystemParameters.PrimaryScreenWidth, sh = SystemParameters.PrimaryScreenHeight;
+                if (w > sw || w > sh)
+                    w = Math.Min(sw, sh);
+                this.Width = this.Height = w;
+            }
+            else if (e.Key == Key.OemMinus)
+            {
+                if (this.Width > 20)
+                {
+                    this.Width = this.Height = this.Width / 1.5;
+                }
+            }
+            else if (e.Key == Key.Right)
+            {
+                if (_viewModel.VerticalRotationDegree < 50)
+                    _viewModel.VerticalRotationDegree += 5;
+            }
+            else if (e.Key == Key.Left)
+            {
+                if (_viewModel.VerticalRotationDegree > -50)
+                    _viewModel.VerticalRotationDegree -= 5;
+            }
+            else if (e.Key == Key.Down)
+            {
+                if (_viewModel.HorizontalRotationDegree < 50)
+                    _viewModel.HorizontalRotationDegree += 5;
+            }
+            else if (e.Key == Key.Up)
+            {
+                if (_viewModel.HorizontalRotationDegree > -50)
+                    _viewModel.HorizontalRotationDegree -= 5;
+            }
+        }
+
+        public void ResetTransforms()
+        {
+            _viewModel.HorizontalRotationDegree = 0;
+            _viewModel.VerticalRotationDegree = -17;
+            Width = 250;
+            Height = 250;
         }
     }
 }
---
+++ SepidarSidebar/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SG.SepidarSidebar
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private double _verticalRotationDegree = -17;
+        private double _horizontalRotationDegree = 0;
+
+        public double VerticalRotationDegree
+        {
+            get { return _verticalRotationDegree; }
+            set
+            {
+                if (_verticalRotationDegree != value)
+                {
+                    _verticalRotationDegree = value;
+                    OnPropertyChanged("VerticalRotationDegree");
+                }
+            }
+        }
+
+        public double HorizontalRotationDegree
+        {
+            get { return _horizontalRotationDegree; }
+            set
+            {
+                if (_horizontalRotationDegree != value)
+                {
+                    _horizontalRotationDegree = value;
+                    OnPropertyChanged("HorizontalRotationDegree");
+                }
+            }
+        }
+
+    }
+}
